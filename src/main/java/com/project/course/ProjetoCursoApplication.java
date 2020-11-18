72fbd4fe9297b00b7eccd6f47dfbd58781f6e1e6
Adriano Costa <adrianocostaads@gmail.com>
2020-11-18 20:29:53 -0300

    Project Created

---
+++ src/main/java/com/project/course/ProjetoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.project.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoApplication.class, args);
+	}
+
+}
